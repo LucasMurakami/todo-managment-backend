ccf649fa1163104cb8af15aebdd31a8878ee1c6d
Lucas Murakami <lucaskaitomurakami@gmail.com>
2023-08-02 11:08:28 -0300

    Projeto iniciado

---
+++ src/main/java/com/lucaskaitomurakami/todomanagment/TodoManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.lucaskaitomurakami.todomanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucaskaitomurakami/todomanagment/TodoManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaskaitomurakami.todomanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
